c97cfef750e528177271004005106022d228cdf4
hatem.zidi <hatem.zidi@gmail.com>
2015-11-13 18:13:45 +0100

    init project from existing version

---
+++ js/backend.js
@@ -0,0 +1,176 @@
+/**
+ * Created with PhpStorm.
+ * User: h.zidi
+ * Date: 04/11/13
+ * Time: 16:43
+ *
+ */
+
+var swatches1 = ['#332244', '#BB3355', '#998899', '#FF5555', '#FFDD99'];
+var swatches2 = ['#445555', '#889999', '#4499DD', '#DDDDDD', '#AACCCC'];
+var swatches3 = ['#3366BB', '#CCCCCC', '#EEEEEE', '#DDDDEE', '#FFDD11'];
+var swatches4 = ['#4D90FE', '#DD4B39', '#F1F1F1', '#777777'];
+var swatches5 = ['#332244', '#5566AA', '#AA7799', '#DD9999', '#FFDD99'];
+
+var SunriseSunsetLayer;
+
+function getMap() {
+
+
+    $('#gmap-marker').gmap({'zoom': 2, 'center': '20,0', 'disableDefaultUI': true}).bind('init', function (e, map) {
+        $.getJSON('../_api_map.php', function (data) {
+
+            $.each(data, function (i, item) {
+                var content = item.city === '' ? '?' : '<img src="images/blank.gif" class="flag flag-' + item.isoCode + '" /> ' + item.city;
+
+                content += ' <i class="fa fa-thumbs-o-down"></i> ' + item.counter;
+
+                $('#gmap-marker').gmap('addMarker', {
+                    'position': new google.maps.LatLng(parseInt(item.latitude), parseInt(item.longitude)),
+                    'icon': new google.maps.MarkerImage("images/markers/marker_red.png"),
+                    'value': parseInt(item.counter),
+                    'visible': true
+                }).click(function () {
+                    $('#gmap-marker').gmap('openInfoWindow', {'content': content, maxWidth: 200}, this);
+                });
+            });
+
+
+            sunriseSunsetLayer = new SunriseSunsetLayer(map, 'GOOGLE');
+            sunriseSunsetLayer.autoUpdate = true;
+            sunriseSunsetLayer.draw();
+
+        });
+
+
+        // $('#map_canvas').gmap('set', 'MarkerClusterer', new MarkerClusterer(map, $(this).gmap('get', 'markers')));
+    });
+}
+
+
+function getMapLarge() {
+
+    var datum;
+
+    $.getJSON('../_api_map_large.php', function (data) {
+        datum = data;
+        google.setOnLoadCallback(drawRegionsMap)
+    });
+
+    function drawRegionsMap() {
+
+        var data = google.visualization.arrayToDataTable(datum);
+
+        var options = {};
+
+        var chart = new google.visualization.GeoChart(document.getElementById('gmap-marker'));
+
+        chart.draw(data, options);
+    }
+
+}
+
+
+function getCounters() {
+    if ($('#dislikes').length) {
+        $.getJSON('../_api_counters.php', function (stream) {
+            Morris.Donut({
+                element: 'dislikes',
+                data: [
+                    {value: stream.total, label: 'dislikes'}
+                ],
+                colors: swatches3,
+                labelColor: '#fff',
+                backgroundColor: '#1CAF9A'
+            });
+
+
+            Morris.Donut({
+                element: 'referrers',
+                data: stream.referrers,
+                colors: swatches1,
+                labelColor: '#fff',
+                backgroundColor: '#F0AD4E'
+            });
+
+
+            Morris.Donut({
+                element: 'origins',
+                data: stream.origins,
+                colors: swatches5,
+                labelColor: '#fff',
+                backgroundColor: '#428BCA'
+            });
+
+            /* Morris.Donut({
+             element: 'browsers',
+             data: stream.agent.browsers,
+             colors: swatches4,
+             labelColor: '#fff',
+             backgroundColor: '#428BCA'
+             });
+             */
+            Morris.Donut({
+                element: 'os',
+                data: stream.agent.os,
+                colors: swatches2,
+                labelColor: '#fff',
+                backgroundColor: '#1D2939'
+            });
+
+        });
+    }
+}
+
+
+function getStats() {
+    if ($('#hourly').length) {
+        $.getJSON('../_api_stats.php', function (stream) {
+            Morris.Bar({
+                element: 'hourly',
+                data: stream.hourstats,
+                xkey: 'h',
+                ykeys: ['v'],
+                labels: ['dislikes'],
+                barRatio: 0.4,
+                xLabelAngle: 35,
+                hideHover: 'auto'
+            });
+
+            Morris.Line({
+                element: 'origins_daily',
+                data: stream.origins,
+                xkey: 'dh',
+                ykeys: ['comment', 'chat'],
+                labels: ['comments', 'chatbox'],
+                parseTime: false,
+                hideHover: 'auto'
+            });
+
+
+            Morris.Area({
+                element: 'daily',
+                data: stream.daystats,
+                xkey: 'dh',
+                ykeys: ['v'],
+                parseTime: false,
+                labels: ['dislikes']
+            });
+
+            Morris.Bar({
+                element: 'weekly',
+                data: stream.weekstats,
+                xkey: 'dh',
+                ykeys: ['v'],
+                parseTime: false,
+                labels: ['dislikes']
+            });
+        });
+    }
+}
+
+function today() {
+    $('.today.time').html(moment().format("H:mm A zz"));
+    $('.today.date').html(moment().format("ddd, MMM Do"));
+}
+
---
+++ js/custom.js
@@ -0,0 +1,225 @@
+
+jQuery(window).load(function() {
+
+   // Page Preloader
+   jQuery('#status').fadeOut();
+   jQuery('#preloader').delay(350).fadeOut(function(){
+      jQuery('body').delay(350).css({'overflow':'visible'});
+   });
+});
+
+jQuery(document).ready(function() {
+
+   // Toggle Left Menu
+   jQuery('.nav-parent > a').click(function() {
+
+      var parent = jQuery(this).parent();
+      var sub = parent.find('> ul');
+
+      // Dropdown works only when leftpanel is not collapsed
+      if(!jQuery('body').hasClass('leftpanel-collapsed')) {
+         if(sub.is(':visible')) {
+            sub.slideUp(200, function(){
+               parent.removeClass('nav-active');
+               jQuery('.mainpanel').css({height: ''});
+               adjustmainpanelheight();
+            });
+         } else {
+            closeVisibleSubMenu();
+            parent.addClass('nav-active');
+            sub.slideDown(200, function(){
+               adjustmainpanelheight();
+            });
+         }
+      }
+      return false;
+   });
+
+   function closeVisibleSubMenu() {
+      jQuery('.nav-parent').each(function() {
+         var t = jQuery(this);
+         if(t.hasClass('nav-active')) {
+            t.find('> ul').slideUp(200, function(){
+               t.removeClass('nav-active');
+            });
+         }
+      });
+   }
+
+   function adjustmainpanelheight() {
+      // Adjust mainpanel height
+      var docHeight = jQuery(document).height();
+      if(docHeight > jQuery('.mainpanel').height())
+         jQuery('.mainpanel').height(docHeight);
+   }
+
+
+   // Tooltip
+   jQuery('.tooltips').tooltip({ container: 'body'});
+
+   // Popover
+   jQuery('.popovers').popover();
+
+   // Close Button in Panels
+   jQuery('.panel .panel-close').click(function(){
+      jQuery(this).closest('.panel').fadeOut(200);
+      return false;
+   });
+
+   // Form Toggles
+   jQuery('.toggle').toggles({on: true});
+
+   // Minimize Button in Panels
+   jQuery('.minimize').click(function(){
+      var t = jQuery(this);
+      var p = t.closest('.panel');
+      if(!jQuery(this).hasClass('maximize')) {
+         p.find('.panel-body, .panel-footer').slideUp(200);
+         t.addClass('maximize');
+         t.html('&plus;');
+      } else {
+         p.find('.panel-body, .panel-footer').slideDown(200);
+         t.removeClass('maximize');
+         t.html('&minus;');
+      }
+      return false;
+   });
+
+
+   // Add class everytime a mouse pointer hover over it
+   jQuery('.nav-bracket > li').hover(function(){
+      jQuery(this).addClass('nav-hover');
+   }, function(){
+      jQuery(this).removeClass('nav-hover');
+   });
+
+
+   // Menu Toggle
+   jQuery('.menutoggle').click(function(){
+
+      var body = jQuery('body');
+      var bodypos = body.css('position');
+
+      if(bodypos != 'relative') {
+
+         if(!body.hasClass('leftpanel-collapsed')) {
+            body.addClass('leftpanel-collapsed');
+            jQuery('.nav-bracket ul').attr('style','');
+
+            jQuery(this).addClass('menu-collapsed');
+
+         } else {
+            body.removeClass('leftpanel-collapsed chat-view');
+            jQuery('.nav-bracket li.active ul').css({display: 'block'});
+
+            jQuery(this).removeClass('menu-collapsed');
+
+         }
+      } else {
+
+         if(body.hasClass('leftpanel-show'))
+            body.removeClass('leftpanel-show');
+         else
+            body.addClass('leftpanel-show');
+
+         adjustmainpanelheight();
+      }
+
+   });
+
+
+
+//   reposition_searchform();
+
+   jQuery(window).resize(function(){
+
+      if(jQuery('body').css('position') == 'relative') {
+
+         jQuery('body').removeClass('leftpanel-collapsed chat-view');
+
+      } else {
+
+         jQuery('body').removeClass('chat-relative-view');
+         jQuery('body').css({left: '', marginRight: ''});
+      }
+
+//      reposition_searchform();
+
+   });
+
+//   function reposition_searchform() {
+//      if(jQuery('.searchform').css('position') == 'relative') {
+//         jQuery('.searchform').insertBefore('.leftpanelinner .userlogged');
+//      } else {
+//         jQuery('.searchform').insertBefore('.header-right');
+//      }
+//   }
+
+
+//   // Sticky Header
+//   if(jQuery.cookie('sticky-header'))
+//      jQuery('body').addClass('stickyheader');
+//
+//   // Sticky Left Panel
+//   if(jQuery.cookie('sticky-leftpanel')) {
+//      jQuery('body').addClass('stickyheader');
+//      jQuery('.leftpanel').addClass('sticky-leftpanel');
+//   }
+//
+//   // Left Panel Collapsed
+//   if(jQuery.cookie('leftpanel-collapsed')) {
+//      jQuery('body').addClass('leftpanel-collapsed');
+//      jQuery('.menutoggle').addClass('menu-collapsed');
+//   }
+//
+//   // Changing Skin
+//   var c = jQuery.cookie('change-skin');
+//   if(c) {
+//      jQuery('head').append('<link id="skinswitch" rel="stylesheet" href="css/style.'+c+'.css" />');
+//   }
+
+    $('.range').each(function(){
+        var cls      = $(this).attr('class');
+        var matches  = cls.split(/([a-zA-Z]+)\-([0-9]+)/g);
+        var elem     = $(this).parent();
+        var options  = {};
+        var input    = elem.find('input');
+
+        elem.append('<div class="uirange"></div>');
+
+        for(i in matches){
+            i = i * 1;
+            if(matches[i] == 'min'){
+                options.min  = matches[i+1]*1;
+            }
+            if(matches[i] == 'max'){
+                options.max  = matches[i+1]*1;
+            }
+        }
+
+        options.slide = function(event, ui){
+            elem.find('label span').empty().append(ui.value);
+            input.val(ui.value);
+
+            $.each( $('#gmap-marker').gmap('get', 'markers'), function (i, m) {
+                if (ui.value != 0 )
+                    m.setVisible( ui.value == m.get('value'));
+                else
+                    m.setVisible(true);
+            });
+
+        }
+        options.value = input.val();
+        options.range = 'min';
+        elem.find('.uirange').slider(options);
+
+        elem.find('label span').empty().append(input.val());
+        input.hide();
+    });
+
+
+    //getMap();
+    getCounters();
+    getStats();
+    today();
+});
